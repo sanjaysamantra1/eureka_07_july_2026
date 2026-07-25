0ece3b1cb82547b28f9fc4f594907113e2758866
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-07-25 21:12:23 +0530

    assignments

--- angular/angular_project_1/src/app/components/databinding/databinding.ts
+++ angular/angular_project_1/src/app/components/databinding/databinding.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-databinding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './databinding.html',
   styleUrl: './databinding.css',
 })
