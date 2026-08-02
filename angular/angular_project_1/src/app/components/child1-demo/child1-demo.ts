2fcb5a0a8f11cdb2702ad03a4b480f19831ad7bd
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-02 20:21:18 +0530

    lifecycle hooks

--- angular/angular_project_1/src/app/components/child1-demo/child1-demo.ts
+++ angular/angular_project_1/src/app/components/child1-demo/child1-demo.ts
@@ -5,9 +5,37 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './child1-demo.html',
   styleUrl: './child1-demo.css',
-  inputs: ['a','b'],
+  inputs: ['a', 'b'],
 })
 export class Child1Demo {
   a: any;
   b: any;
+
+  constructor() {
+    console.log('child constructor');
+  }
+  ngOnChanges() {
+    console.log('child ngOnChanges');
+  }
+  ngOnInit() {
+    console.log('child ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('child ngDoCheck');
+  }
+  // ngAfterContentInit() {
+  //   console.log('child ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('child ngAfterContentChecked')
+  // }
+  // ngAfterViewInit() {
+  //   console.log('child ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('child ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('child ngOnDestory');
+  // }
 }
