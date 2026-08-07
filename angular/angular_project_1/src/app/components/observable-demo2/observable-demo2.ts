227a815497f02ead6ce6531918ce942636e7b848
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-07 20:24:38 +0530

    aug 7

--- angular/angular_project_1/src/app/components/observable-demo2/observable-demo2.ts
+++ angular/angular_project_1/src/app/components/observable-demo2/observable-demo2.ts
@@ -1,16 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { from, range } from 'rxjs';
+import { filter, from, interval, map, range } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo2',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './observable-demo2.html',
   styleUrl: './observable-demo2.css',
 })
 export class ObservableDemo2 {
+  numbers = interval(1000);
+
+  // when you want to create 1 observable out of another observable, we use pipe
+  even_numbers = this.numbers.pipe(filter((val) => val % 2 == 0));
+  square_numbers = this.numbers.pipe(map((val) => val * val));
+
+  // digital clock, Note: observable varibales should end with $
+  currentTime$ = interval(1000).pipe(map(() => new Date().toLocaleTimeString()));
+
   ngOnInit() {
     // this.from_demo();
-    this.range_demo();
+    // this.range_demo();
   }
 
   from_demo() {
@@ -22,9 +32,15 @@ export class ObservableDemo2 {
   range_demo() {
     let num_Obs = range(10, 5);
     num_Obs.subscribe({
-        next : (response)=>{console.log('Response: ',response)},
-        error : (err)=>{console.log(err)},
-        complete : ()=>{console.log('All data Received')},
-    })
+      next: (response) => {
+        console.log('Response: ', response);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('All data Received');
+      },
+    });
   }
 }
