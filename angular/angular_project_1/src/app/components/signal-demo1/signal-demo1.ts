22347367f49e147fe10a2b33033349fcecdd1d31
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-09 20:15:47 +0530

    signal

--- angular/angular_project_1/src/app/components/signal-demo1/signal-demo1.ts
+++ angular/angular_project_1/src/app/components/signal-demo1/signal-demo1.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-demo1',
@@ -14,5 +14,26 @@ export class SignalDemo1 {
   updateCookieCount(event: Event) {
     const input = event.target as HTMLInputElement;
     this.cookieCount.set(parseInt(input.value));
+  }
+
+  // ==========================
+  
+  num: WritableSignal<number> = signal(0);
+  messages: WritableSignal<string[]> = signal([]);
+  numDouble: Signal<number> = computed(() => this.num() * 2);
+  numSquare: Signal<number> = computed(() => this.num() * this.num());
+
+  increment() {
+    this.num.update((value: number) => value + 1);
+    this.messages.set([...this.messages(), `Value of Num is: ${this.num()}`]);
+  }
+  decrement() {
+    this.num.update((val: number) => val - 1);
+    this.messages().pop();
+    this.messages.set([...this.messages()]);
+  }
+  reset() {
+    this.num.set(0);
+    this.messages.set([]);
   }
 }
